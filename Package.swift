a9c9624589083f1407843ac32c6ed4fb76348804
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-12-02 00:21:28 +0300

    Migrate to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -20,25 +20,14 @@ let package = Package(
             name: "PostgreSQL",
             dependencies: [
                 .product(name: "Network", package: "network"),
-            ],
-            swiftSettings: swift6),
+            ]),
         .testTarget(
             name: "Tests",
             dependencies: [
                 .target(name: "PostgreSQL"),
-            ],
-            swiftSettings: swift6),
+            ]),
     ]
 )
-
-let swift6: [SwiftSetting] = [
-    .enableUpcomingFeature("ConciseMagicFile"),
-    .enableUpcomingFeature("ForwardTrailingClosures"),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("StrictConcurrency"),
-    .enableUpcomingFeature("ImplicitOpenExistentials"),
-    .enableUpcomingFeature("BareSlashRegexLiterals"),
-]
 
 // MARK: - custom package source
 
